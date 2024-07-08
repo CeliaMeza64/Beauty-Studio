4555cc1a90c05c230f25b492539aa706440ddb0d
TelmaMerlo <telma.merlo@unah.hn>
2024-07-07 21:42:42 -0600

    tablas

--- database/migrations/2024_07_03_211806_create_categorias_table.php
+++ database/migrations/2024_07_03_211806_create_categorias_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('servicios', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre'); 
-            $table->text('descripcion');
-            $table->string('imagen');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('servicios');
+        Schema::dropIfExists('categorias');
     }
 };
