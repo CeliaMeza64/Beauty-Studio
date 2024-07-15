e98da1523e045305d5d260ba7e1f4b750084ec6f
CeliaMeza64 <celia.meza@unah.hn>
2024-07-15 15:16:39 -0600

    actualizar

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -106,10 +106,7 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="home">Administrador</a>
-        </li>  
-        <li class="nav-item">
-          <a class="nav-link" href="home">Agenda</a>
-        </li>   
+        </li>    
       </ul>
     </div>
   </div>
